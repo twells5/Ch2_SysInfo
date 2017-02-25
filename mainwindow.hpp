ab12b94a7c583c95a284935ac5475d78dd5c024d
Timothy Wells <twells@Timothys-MacBook-Pro.local>
2017-02-24 21:50:39 -0500

    Initial code

---
+++ mainwindow.hpp
@@ -0,0 +1,24 @@
+#ifndef MAINWINDOW_HPP
+#define MAINWINDOW_HPP
+
+#include <QMainWindow>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+    // Test comment.
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+};
+
+#endif // MAINWINDOW_HPP
